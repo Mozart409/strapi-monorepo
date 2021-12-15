70e3fcd621e1a6b8604de92a6f080db982f214b9
mozart409 <38767929+Mozart409@users.noreply.github.com>
2021-12-15 14:59:58 +0100

    fix: :lipstick: fixed mobile nav layout

--- frontend/components/elements/mobile-nav-menu.tsx
+++ frontend/components/elements/mobile-nav-menu.tsx
@@ -26,12 +26,11 @@ const MobileNavMenu = ({ navbar, closeSelf }: MobileNavMenuProps) => {
   useLockBodyScroll()
 
   return (
-    <div className="overflow-y-scroll fixed top-0 left-0 z-10 py-6 pb-6 w-screen h-screen bg-white border-b-2 border-gray-200 sm:py-2">
-      <div className="container grid grid-cols-3 grid-flow-row justify-between items-center">
+    <div className="overflow-y-scroll fixed top-0 left-0 z-10 pb-6 w-screen h-screen bg-white">
+      <div className="container flex flex-col justify-between">
         {/* Top section */}
-        <div></div>
-        {/* Company logo */}
-        <div className="justify-self-center">
+        <div className="grid grid-flow-row grid-cols-3 justify-between items-center">
+          <div></div>
           {navbar.logo ? (
             <CustomImage
               media={navbar?.logo}
@@ -47,25 +46,25 @@ const MobileNavMenu = ({ navbar, closeSelf }: MobileNavMenuProps) => {
               height={64}
             />
           )}
-        </div>
-        {/* Close button */}
-        <div className="justify-self-end">
-          <button onClick={closeSelf} className="py-1 px-1">
-            <div className="w-auto h-8">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-          </button>
+          {/* Close button */}
+          <div className="block justify-self-end p-1 mr-1">
+            <button onClick={closeSelf} className="py-1 px-1">
+              <div className="w-auto h-8">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </button>
+          </div>
         </div>
 
         {/* Bottom section */}
@@ -98,12 +97,12 @@ const MobileNavMenu = ({ navbar, closeSelf }: MobileNavMenuProps) => {
               </li>
             ))}
           </ul>
-          {navbar.button ? (
+          {navbar.button && (
             <ButtonLink
               button={navbar.button}
               appearance={getButtonAppearance(navbar.button.type, 'light')}
             />
-          ) : null}
+          )}
         </div>
       </div>
     </div>
